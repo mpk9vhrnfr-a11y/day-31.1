eced8f22dfec56f5e2e06035dfc4eb19851f24fe
sanaa <Sanaakumar07@gmail.com>
2025-11-26 12:09:34 +0530

    c

--- day 31.1/main.c
+++ day 31.1/main.c
@@ -1,15 +1,24 @@
-//
-//  main.c
-//  day 31.1
-//
-//  Created by Sanaa Kumar on 26/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int arr[100], n, i, key, found = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            found = 1;
+            break;
+        }
+    }
+    if(found == 1) {
+        printf("Element %d found at position %d\n", key, i + 1);
+    } else {
+        printf("Element %d not found in the array\n", key);
+    }
+    return 0;
 }
